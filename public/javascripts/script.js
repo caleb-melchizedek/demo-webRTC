8a2985f51de2ad7c064ef9a7ffc5dfb00070a1ee
caleb melchizedek <caleb.melchizedek@acity.edu.gh>
2021-04-27 11:57:04 +0000

    wrapped js in load event listener

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,220 +1,224 @@
 //require('./mediaDevicesPolyfill')
 
 //const port = normalizePort(process.env.PORT || '3000');
-const socket =io();
+window.addEventListener('load',()=>{
 
-const { RTCPeerConnection, RTCSessionDescription } = window;
-const peerConnection= new RTCPeerConnection;
 
-//capture local media
+  const socket =io();
 
-const mediaCheck={
-  audio:false,
-  video:false
-};
-var constraints={
-  audio:true,
-  video:true
-};
+  const { RTCPeerConnection, RTCSessionDescription } = window;
+  const peerConnection= new RTCPeerConnection;
 
-var mySocketId;
+  //capture local media
 
-function getMedia(socketId){
-  navigator.mediaDevices.getUserMedia(constraints)
-  .then(
-    stream => {
-      window.stream = stream;
+  const mediaCheck={
+    audio:false,
+    video:false
+  };
+  var constraints={
+    audio:true,
+    video:true
+  };
 
-      if(mediaCheck.audio==true && mediaCheck.video==true){
-        stream.getTracks().forEach(function(track){
-          console.log(track);
-        })
-      }
-      else if(mediaCheck.audio==false && mediaCheck.video==true){
-        // stop only mic
-        stream.getTracks().forEach(function(track) {
-            if (track.readyState == 'live' && track.kind === 'audio') {
-                track.stop();
-                // track.enabled=false;
-            }
+  var mySocketId;
+
+  function getMedia(socketId){
+    navigator.mediaDevices.getUserMedia(constraints)
+    .then(
+      stream => {
+        window.stream = stream;
+
+        if(mediaCheck.audio==true && mediaCheck.video==true){
+          stream.getTracks().forEach(function(track){
             console.log(track);
-        });
-      }
-      else if(mediaCheck.audio==true && mediaCheck.video==false){
-         // stop only camera
-          stream.getTracks().forEach(function(track) {
-              if (track.readyState == 'live' && track.kind === 'video') {
-                  track.stop();
-                  // track.enabled=false;
-              }
-              console.log(track);
-          });
-      }
-      else if(mediaCheck.audio==false && mediaCheck.video==false){
-        // stop both camera and mic
-          stream.getTracks().forEach(function(track) {
-              if (track.readyState == 'live') {
-                  track.stop();
-                  // track.enabled=false;
-              }
-              console.log(track);
-          });
-      }
-      
-  
-
-      const localVideo = document.querySelector(`#${mySocketId}`);
-      console.log(localVideo);
-      if (localVideo) { 
-        localVideo.muted=true;
-        if ("srcObject" in localVideo) {
-          localVideo.srcObject = stream;
-        } else {
-          // Avoid using this in new browsers, as it is going away.
-          localVideo.src = window.URL.createObjectURL(stream);
+          })
         }
-      };
-      stream.getTracks().forEach(track => peerConnection.addTrack(track, stream));
-    }
-  ).catch(
-    error => {console.warn(error.message) }
-  );;
-}
+        else if(mediaCheck.audio==false && mediaCheck.video==true){
+          // stop only mic
+          stream.getTracks().forEach(function(track) {
+              if (track.readyState == 'live' && track.kind === 'audio') {
+                  track.stop();
+                  // track.enabled=false;
+              }
+              console.log(track);
+          });
+        }
+        else if(mediaCheck.audio==true && mediaCheck.video==false){
+          // stop only camera
+            stream.getTracks().forEach(function(track) {
+                if (track.readyState == 'live' && track.kind === 'video') {
+                    track.stop();
+                    // track.enabled=false;
+                }
+                console.log(track);
+            });
+        }
+        else if(mediaCheck.audio==false && mediaCheck.video==false){
+          // stop both camera and mic
+            stream.getTracks().forEach(function(track) {
+                if (track.readyState == 'live') {
+                    track.stop();
+                    // track.enabled=false;
+                }
+                console.log(track);
+            });
+        }
+        
+    
 
-
-//socket operations
-
-socket.on("update-user-list", ({ users,myId }) => {
-  mySocketId=myId
-  updateUserList(users);
- });
-
- 
-
- socket.on("remove-user", ({ socketId }) => {
-  const elToRemove = document.getElementById(socketId);
-  
-  if (elToRemove) {
-    elToRemove.remove();
+        const localVideo = document.querySelector(`#${mySocketId}`);
+        console.log(localVideo);
+        if (localVideo) { 
+          localVideo.muted=true;
+          if ("srcObject" in localVideo) {
+            localVideo.srcObject = stream;
+          } else {
+            // Avoid using this in new browsers, as it is going away.
+            localVideo.src = window.URL.createObjectURL(stream);
+          }
+        };
+        stream.getTracks().forEach(track => peerConnection.addTrack(track, stream));
+      }
+    ).catch(
+      error => {console.warn(error.message) }
+    );;
   }
- });
 
 
- //functions
+  //socket operations
 
- //toggle video
-let videoBtn=document.getElementById("videoBtn");
-videoBtn.addEventListener("click",
-  function(){    
-    mediaCheck.video= !mediaCheck.video 
-    console.log(mediaCheck)
-    //socket.emit("getMedia");
-    getMedia(mySocketId);
-  }
-
-);
-//toggle audio
-let micBtn=document.getElementById("micBtn");
-micBtn.addEventListener( "click",
-  function(){ 
-    mediaCheck.audio= !mediaCheck.audio 
-    console.log(mediaCheck)
-    //socket.emit("getMedia");
-    getMedia(mySocketId);
-  }
-);
-
- function updateUserList(users) {
-  //otherS
-  console.log(users);
-  const activeUserContainer = document.getElementById("smallVideosWrapper")
-
-  while (activeUserContainer.firstChild) {
-    activeUserContainer.removeChild(activeUserContainer.firstChild);
-    console.log("vids removed for replacement")
-  }
-  users.forEach(user => {
-    const alreadyExistingUser = document.getElementById(user.name);
-    if (!alreadyExistingUser) {
-      const userContainerEl = createUserItemContainer(user.socketId, user.name);
-      activeUserContainer.appendChild(userContainerEl); 
-    }
-  });
-  console.log("User list was just updated");
- }
-
- function createUserItemContainer(socketId, name) {
-  var inits="";
-  let a =name.split(" ",2);
-  a.forEach(function(b){
-    inits=inits+b[0].toUpperCase();
+  socket.on("update-user-list", ({ users,myId }) => {
+    mySocketId=myId
+    updateUserList(users);
   });
 
-  console.log(inits);
-  const smallVideoWrapper = document.createElement("div");
-  const smallVideo = document.createElement("video");
-
-  smallVideoWrapper.setAttribute("class", "wrapper");
-  smallVideoWrapper.setAttribute("id",name );
-  smallVideoWrapper.setAttribute("data-initials",inits);
-
- // smallVideo.setAttribute("class", "");
- 
-  smallVideo.setAttribute("id",socketId);
-  smallVideo.setAttribute("autoplay","true");
-  
   
 
-  
-
-  smallVideoWrapper.appendChild(smallVideo);
-  
-  smallVideoWrapper.addEventListener("click", 
-    () => {
-      callUsers(socketId);
+  socket.on("remove-user", ({ socketId }) => {
+    const elToRemove = document.getElementById(socketId);
+    
+    if (elToRemove) {
+      elToRemove.remove();
     }
-  ); 
-  return smallVideoWrapper;
- }
-
-
- //RTC functions
-
- async function callUsers(socketId) {
-  const offer = await peerConnection.createOffer();
-  await peerConnection.setLocalDescription(new RTCSessionDescription(offer));
-  
-  socket.emit("call-users", {
-    offer
   });
- } 
- 
- socket.on("call-made", async data => {
-  await peerConnection.setRemoteDescription(
-    new RTCSessionDescription(data.offer)
+
+
+  //functions
+
+  //toggle video
+  let videoBtn=document.getElementById("videoBtn");
+  videoBtn.addEventListener("click",
+    function(){    
+      mediaCheck.video= !mediaCheck.video 
+      console.log(mediaCheck)
+      //socket.emit("getMedia");
+      getMedia(mySocketId);
+    }
+
   );
-  const answer = await peerConnection.createAnswer();
-  await peerConnection.setLocalDescription(new RTCSessionDescription(answer));
-  
-  socket.emit("make-answer", {
-    answer,
-    to: data.socket
-  });
- });
-
-
- socket.on("answer-made", async data=>{
-  await peerConnection.setRemoteDescription(
-    new RTCSessionDescription(data.answer)
+  //toggle audio
+  let micBtn=document.getElementById("micBtn");
+  micBtn.addEventListener( "click",
+    function(){ 
+      mediaCheck.audio= !mediaCheck.audio 
+      console.log(mediaCheck)
+      //socket.emit("getMedia");
+      getMedia(mySocketId);
+    }
   );
- 
- });
 
- peerConnection.ontrack = function({ streams:[stream] }) {
-   const remoteStream= new MediaStream();
-   remoteStream.addTrack(event.track ,stream);
-  const remoteVideo = document.getElementById("remote-video");
-  if (remoteVideo) {
-    remoteVideo.srcObject =remoteStream;
+  function updateUserList(users) {
+    //otherS
+    console.log(users);
+    const activeUserContainer = document.getElementById("smallVideosWrapper")
+
+    while (activeUserContainer.firstChild) {
+      activeUserContainer.removeChild(activeUserContainer.firstChild);
+      console.log("vids removed for replacement")
+    }
+    users.forEach(user => {
+      const alreadyExistingUser = document.getElementById(user.name);
+      if (!alreadyExistingUser) {
+        const userContainerEl = createUserItemContainer(user.socketId, user.name);
+        activeUserContainer.appendChild(userContainerEl); 
+      }
+    });
+    console.log("User list was just updated");
   }
- }; 
+
+  function createUserItemContainer(socketId, name) {
+    var inits="";
+    let a =name.split(" ",2);
+    a.forEach(function(b){
+      inits=inits+b[0].toUpperCase();
+    });
+
+    console.log(inits);
+    const smallVideoWrapper = document.createElement("div");
+    const smallVideo = document.createElement("video");
+
+    smallVideoWrapper.setAttribute("class", "wrapper");
+    smallVideoWrapper.setAttribute("id",name );
+    smallVideoWrapper.setAttribute("data-initials",inits);
+
+  // smallVideo.setAttribute("class", "");
+  
+    smallVideo.setAttribute("id",socketId);
+    smallVideo.setAttribute("autoplay","true");
+    
+    
+
+    
+
+    smallVideoWrapper.appendChild(smallVideo);
+    
+    smallVideoWrapper.addEventListener("click", 
+      () => {
+        callUsers(socketId);
+      }
+    ); 
+    return smallVideoWrapper;
+  }
+
+
+  //RTC functions
+
+  async function callUsers(socketId) {
+    const offer = await peerConnection.createOffer();
+    await peerConnection.setLocalDescription(new RTCSessionDescription(offer));
+    
+    socket.emit("call-users", {
+      offer
+    });
+  } 
+  
+  socket.on("call-made", async data => {
+    await peerConnection.setRemoteDescription(
+      new RTCSessionDescription(data.offer)
+    );
+    const answer = await peerConnection.createAnswer();
+    await peerConnection.setLocalDescription(new RTCSessionDescription(answer));
+    
+    socket.emit("make-answer", {
+      answer,
+      to: data.socket
+    });
+  });
+
+
+  socket.on("answer-made", async data=>{
+    await peerConnection.setRemoteDescription(
+      new RTCSessionDescription(data.answer)
+    );
+  
+  });
+
+  peerConnection.ontrack = function({ streams:[stream] }) {
+    const remoteStream= new MediaStream();
+    remoteStream.addTrack(event.track ,stream);
+    const remoteVideo = document.getElementById("remote-video");
+    if (remoteVideo) {
+      remoteVideo.srcObject =remoteStream;
+    }
+  }; 
+})
